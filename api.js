f5c3c81288b563a8bbfbcbf9df57cce6f69c0602
Cedric Gasser <cedric_gasser@sluz.ch>
2020-11-26 07:14:55 +0100

    Fixed api-Bug

--- api.js
+++ api.js
@@ -11,34 +11,50 @@ card = {
 }
 */
 
-let cards = []
+let cards = [];
 
-let counter = 0
+let counter = 0;
+
+cards = [
+    ...cards,
+    {
+    "id": counter,
+    "text": "Drink some Coffee",
+    "status": "ToDo",
+    }
+];
+
+counter++
 
 router
     .get("/cards", context => context.response.body = cards)
     .get("/cards/:id", context => {
-        const index = cards.findIndex(c => c.id == id);
+        const index = cards.findIndex(c => c.id == context.params.id);
         context.response.body = cards[index]
     })
     .post("/cards", async context => {
         const card = await context.request.body({ type: "json" }).value;
         card.id = counter;
+        counter++;
         cards = [
             ...cards,
             card
         ];
-        counter++;
+        context.response.body = card
     })
     .delete("/cards/:id", context => {
         const id = context.params.id;
         cards = cards.filter(p => p.id != id);
     })
     .put("/cards/:id", async context => {
+        console.log(context.request);
         const card = await context.request.body({ type: "json" }).value;
-        const id = context.params.id;
-        const index = cards.findIndex(c => c.id == id);
+        console.log("id:", context.params.id);
+        console.log("card", card);
+        const index = cards.findIndex(c => c.id == context.params.id);
+        card.id = cards[index].id;
         cards[index] = card;
+        context.response.body = cards[index]
     });
 
 app.use(router.routes());
@@ -48,4 +64,4 @@ app.use(async context => {
       index: "index.html",
     });
   });
-app.listen({ port: 8000 })
+app.listen({ port: 8000 });
